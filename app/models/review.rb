af0bca8c3d59401219012af033f4299e1a8f71a2
Afzal Masra <afzal.masra@csgi.com>
2018-04-30 18:01:43 -0500

    enum added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
       enum relationship:  {peer: 0, self: 1}
-      enum judgement:  {does_not_meet_expectation: 0, partially_meets: 1, fully_meets_expectations: 2 , exceeds_expectations: 3}
-      enum teamwork: {does_not_meet_expectation: 0, partially_meets: 1, fully_meets_expectations: 2 , exceeds_expectations: 3}
-      enum leadership:  {does_not_meet_expectation: 0, partially_meets: 1, fully_meets_expectations: 2 , exceeds_expectations: 3}
-      enum technical: {does_not_meet_expectation: 0, partially_meets: 1, fully_meets_expectations: 2 , exceeds_expectations: 3}
+      enum judgement: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :rating_1
+      enum teamwork: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :rating_2
+      enum leadership: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :rating_3
+      enum technical: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :rating_4
 end

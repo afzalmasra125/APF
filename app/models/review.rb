d28200947a859ea8bc830c7fa2f0d16733cf7285
Afzal Masra <afzal.masra@csgi.com>
2018-05-22 20:01:00 -0500

    change design, routes, added view

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
       enum relationship:  {peer: 0, self: 1}
-      enum judgement: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :rating_1
-      enum teamwork: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :rating_2
-      enum leadership: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :rating_3
-      enum technical: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :rating_4
+      enum judgement: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :judgement
+      enum teamwork: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :teamwork
+      enum leadership: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :leadership
+      enum technical: [:does_not_meet_expectation, :partially_meets, :fully_meets_expectations, :exceed_expectations ], _prefix: :technical
 end

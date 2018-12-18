a1c870da0359b66904c8def5bbc4f43d148dd2b8
Jaredh55 <jaredh55@gmail.com>
2018-12-17 19:26:15 -0600

    updated pending reviews page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,7 +5,9 @@ class ReviewsController < ApplicationController
         render json: reviews.as_json
   end 
   def pending_reviews
-      @reviews = Review.where(reviewer_id: nil)
+      @reviews = Review.all
+      # @reviews = Review.all
+
       render 'index.json.jbuilder'
   end
   def create 
@@ -21,7 +23,7 @@ class ReviewsController < ApplicationController
     if reviews.save
       render json: reviews.as_json
     else
-      reviews.errors
+      p reviews.errors
     end
   end
   def show

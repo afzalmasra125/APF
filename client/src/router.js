b344899dd0b05e9e8784ecc208e2acbd3dda18b0
Afzal Masra <afzalmasra@gmail.com>
2019-01-07 19:31:58 -0600

    added logut componet

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import SignUp from './components/SignUp.vue'
 import Login from './components/Login.vue'
+import LogOut from './components/LogOut.vue'
 import HomePage from './components/HomePage.vue'
 
 Vue.use(Router)
@@ -11,7 +12,7 @@ export default new Router({
     { path: "/", component: Login },
     { path: "/home", component: HomePage },
     { path: "/signup", component: SignUp },
-    // { path: "/logout", component: LogoutPage },
+    { path: "/logout", component: LogOut },
     // { path: "/employees", component: EmployeeIndexPage},
     // { path: "/employees/:id", component: ReviewShowPage},
     // { path: "/reviews/new", component: ReviewsNewPage },
